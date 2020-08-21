349e56cd0ed1ab022fd7329a19b1fee7db4b508c
azuregitter <geromeriego@gmail.com>
2020-08-21 08:22:06 +0800

    initial

---
+++ precache-manifest.1774a897852d61da32ae33df7cd2a9aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e982d473bf8d36329f98c8ebf91331ef",
+    "url": "/riegoreactpage/index.html"
+  },
+  {
+    "revision": "d52b0f9e91bdbc193e4a",
+    "url": "/riegoreactpage/static/css/main.7ed0d603.chunk.css"
+  },
+  {
+    "revision": "a9ebd86989aed6d13366",
+    "url": "/riegoreactpage/static/js/2.ad653f06.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/riegoreactpage/static/js/2.ad653f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d52b0f9e91bdbc193e4a",
+    "url": "/riegoreactpage/static/js/main.9e5be979.chunk.js"
+  },
+  {
+    "revision": "bcfd70ec760ffffb7969",
+    "url": "/riegoreactpage/static/js/runtime-main.cd7d024b.js"
+  }
+]);
